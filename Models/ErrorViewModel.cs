40566b8fd1fe7f3fd0067e984af9693d3028bd0d
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-06-09 16:12:51 +0300

    Initial. Version 0.1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MAS_semestral_project_MVS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
