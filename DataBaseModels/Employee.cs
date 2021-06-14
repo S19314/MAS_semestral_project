4989cb80ed1e11c758f584bbf15922e8ba34ccb1
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-06-14 14:02:46 +0300

    Start create Overlapping for Client and Worker. Version 3.1.0

---
+++ DataBaseModels/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MAS_semestral_project_MVS.DataBaseModels
+{
+    public class Employee : Person
+    {
+        public ICollection<String> KnowedLanguages;
+    }
+}
---
+++ DataBaseModels/Receptionist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MAS_semestral_project_MVS.DataBaseModels
+{
+    public class Receptionist
+    {
+        // public ICollection<String> KnowedLanguages;
+
+    }
+}
