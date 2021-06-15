2e8fd3596defdb8e724193bce373b9e1326b9725
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-06-15 19:28:08 +0300

    Created Enum For RelationWithCompany. Version 3.2.2

---
+++ Models/Enums/RelationWithCompanyEnum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MAS_semestral_project_MVS.Models.Enums
+{
+    public class RelationWithCompanyEnum 
+    {
+        public enum RelationWithCompany
+        {
+            Client = 0,
+            Employee = 1
+        }
+        private readonly string[] RelationInString = { "Client", "Employee" };
+        public RelationWithCompanyEnum()
+        {
+        }
+
+        public string GetConformityEnumValue(RelationWithCompany enumValue)
+        {
+            // return  (string)RelationInString.GetValue(((int)enumValue));
+            return RelationInString[(int)enumValue];
+        }
+
+    }
+}
