36d1fd346ae26513ab55e722c327b0ea832e319a
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-06-15 21:47:58 +0300

    CORRECTED create ClassAtributesInColumn; Set nullable for columns; Version 2.4.2

--- SQL_Code/2021_06_15_21_32/MAS_semestral_project_create_ClassAtributesInColumn.sql
+++ SQL_Code/2021_06_15_21_32/MAS_semestral_project_create_ClassAtributesInColumn.sql
@@ -1,16 +1,16 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2021-06-15 18:36:37.207
+-- Last modification date: 2021-06-15 18:46:45.199
 
 -- tables
 -- Table: ClassAttributesInColumn
 CREATE TABLE ClassAttributesInColumn (
     Id int  NOT NULL,
-    EmployeeMaxHourRate decimal(10,2)  NOT NULL,
-    DirectorMaxHourRate decimal(10,2)  NOT NULL,
-    CleanerMaxHourRate decimal(10,2)  NOT NULL,
-    ReceptionistMaxHourRate decimal(10,2)  NOT NULL,
-    Cleaner_Receptionist__MaxHourRate decimal(10,2)  NOT NULL,
-    CleanerMaxToolsQuantity int  NOT NULL,
+    EmployeeMaxHourRate decimal(10,2)  NULL,
+    DirectorMaxHourRate decimal(10,2)  NULL,
+    CleanerMaxHourRate decimal(10,2)  NULL,
+    ReceptionistMaxHourRate decimal(10,2)  NULL,
+    Cleaner_Receptionist__MaxHourRate decimal(10,2)  NULL,
+    CleanerMaxToolsQuantity int  NULL,
     CONSTRAINT ClassAttributesInColumn_pk PRIMARY KEY  (Id)
 );
 
