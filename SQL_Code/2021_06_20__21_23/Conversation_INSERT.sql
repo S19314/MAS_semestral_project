fe8da1b5de58fe08c8c21d859b8c10e52b8b7e4c
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-06-21 00:14:33 +0300

    Added View for OrderList. Version 9.14.2

--- SQL_Code/2021_06_20__21_23/Conversation_INSERT.sql
+++ SQL_Code/2021_06_20__21_23/Conversation_INSERT.sql
@@ -1,5 +1,6 @@
 use MAS_semestral;
 SELECT * FROM Person WHERE RelationWithCompany != 'Client';
+SELECT * FROM Person WHERE IdOsoba in (10,11);
 SELECT * FROM Person WHERE RelationWithCompany = 'Client';
 UPDATE Person 
 SET EmployeeExperienceType = 'Apprentice',
@@ -29,3 +30,7 @@ VALUES;
 SELECT * FROM [MAS_semestral].[dbo].[Order];
 INSERT INTO [MAS_semestral].[dbo].[Order] ( CreationDate, Offer_Id, Osoba_IdOsoba, Osoba_2_IdOsoba)
 VALUES(convert(datetime,'13-08-21 10:54:09 PM',5), 11, 10, 11);
+
+SELECT * FROM [MAS_semestral].[dbo].[Order]
+INNNER JOIN Person AS Client
+ON Client.IdOsoba = Osoba_IdOsoba;
